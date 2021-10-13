32615fb827bdc32265ced702c3d56f414cffdb19
Casey Colella <cpcolella@gmail.com>
2021-10-13 13:20:50 -0700

    Update to Manifest V3
    #major

---
+++ js/service_worker.js
@@ -0,0 +1,27 @@
+chrome.runtime.onInstalled.addListener(function()
+{
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function()
+    {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostEquals: 'github.com' },
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+
+    let eventList = ['onCompleted', 'onReferenceFragmentUpdated', 'onTabReplaced', 'onHistoryStateUpdated'];
+
+    eventList.forEach(function (e) {
+        chrome.webNavigation[e].addListener(function (event) {
+            util.autoApplyItems(event.url);
+        });
+    });
+
+    chrome.tabs.onActivated.addListener(function (event) {
+        chrome.tabs.get(event.tabId, function (tab)
+        {
+            util.autoApplyItems(tab.url);
+        });
+    });
+});
--- js/util.js
+++ js/util.js
@@ -1,101 +1,49 @@
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
-let expandAllCounter = 0;
-
 var util = {
     expandAll: function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let codeText = `const sleep` + (expandAllCounter) + ` = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
-
-                async function Expand() {
-                    var elements = document.querySelectorAll("button.ajax-pagination-btn[type='submit'][data-disable-with]");
-                    var counter = 1;
-                
-                    while (elements.length > 0)
-                    {
-                        console.log("Expansion round " + counter++);
-                
-                        for (var i = 0; i < elements.length; i++)
-                        {
-                            elements[i].click();
-                        }
-                
-                        await sleep` + (expandAllCounter) + `(3000);
-                        console.log("Refreshing elements...");;
-                        elements = document.querySelectorAll('button.ajax-pagination-btn[type="submit"][data-disable-with]');
-                    }
-                
-                    console.log("Complete!");
-                }
-                
-                Expand();`;
-
-            chrome.tabs.executeScript(tabs[0].id, { code: codeText });
-            expandAllCounter++;
+            chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, func: NewUtil.Expand });
         });
     },
     hideResolved: function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let codeText = `var elements = document.querySelectorAll("details[data-resolved='true']");
-                elements.forEach(function(element) { element.setAttribute("style", "display: none !important;"); });`;
-
-            chrome.tabs.executeScript(tabs[0].id, { code: codeText });
+            chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, func: NewUtil.HideResolved });
         });
     },
     showResolved: function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let codeText = `var elements = document.querySelectorAll("details[data-resolved='true']");
-                elements.forEach(function(element) { element.removeAttribute("style"); });`;
-
-            chrome.tabs.executeScript(tabs[0].id, { code: codeText });
+            chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, func: NewUtil.ShowResolved });
         });
     },
     hideCommits: function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let codeText = `var elements = document.querySelectorAll("div.js-commit-group");
-                elements.forEach(function(element) { element.setAttribute("style", "display: none !important;"); });`;
-
-            chrome.tabs.executeScript(tabs[0].id, { code: codeText });
+            chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, func: NewUtil.HideCommits });
         });
     },
     showCommits: function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let codeText = `var elements = document.querySelectorAll("div.js-commit-group");
-                elements.forEach(function(element) { element.removeAttribute("style"); });`;
-
-            chrome.tabs.executeScript(tabs[0].id, { code: codeText });
+            chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, func: NewUtil.ShowCommits });
         });
     },
     hideStatuses: function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let codeText = `var elements = document.querySelectorAll(".js-timeline-item  .TimelineItem[data-team-hovercards-enabled]:not([data-url]), .js-timeline-item > div > div[id|=pullrequestreview] > .TimelineItem:not([data-url]), .js-timeline-item > .TimelineItem:not([data-url]) > span.TimelineItem-badge.bg-blue.text-white")
-                elements.forEach(function(element) { element.setAttribute("style", "display: none !important;"); });`;
-
-            chrome.tabs.executeScript(tabs[0].id, { code: codeText });
+            chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, func: NewUtil.HideStatuses });
         });
     },
     showStatuses: function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let codeText = `var elements = document.querySelectorAll(".js-timeline-item  .TimelineItem[data-team-hovercards-enabled]:not([data-url]), .js-timeline-item > div > div[id|=pullrequestreview] > .TimelineItem:not([data-url]), .js-timeline-item > .TimelineItem:not([data-url]) > span.TimelineItem-badge.bg-blue.text-white")
-                elements.forEach(function(element) { element.removeAttribute("style"); });`;
-
-            chrome.tabs.executeScript(tabs[0].id, { code: codeText });
+            chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, func: NewUtil.ShowStatuses });
         });
     },
     hideComments: function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let codeText = `var elements = document.querySelectorAll(".TimelineItem.js-comment-container, .TimelineItem > .timeline-comment-group")
-                elements.forEach(function(element) { element.setAttribute("style", "display: none !important;"); });`;
-
-            chrome.tabs.executeScript(tabs[0].id, { code: codeText });
+            chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, func: NewUtil.HideComments });
         });
     },
     showComments: function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let codeText = `var elements = document.querySelectorAll(".TimelineItem.js-comment-container, .TimelineItem > .timeline-comment-group")
-                elements.forEach(function(element) { element.removeAttribute("style"); });`;
-
-            chrome.tabs.executeScript(tabs[0].id, { code: codeText });
+            chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, func: NewUtil.ShowComments });
         });
     },
     checkUrl: function(url) {
@@ -136,3 +84,92 @@ var util = {
         });
     }
 };
+
+
+var NewUtil = (function () {
+    'use strict';
+
+    async function Expand()
+    {
+        let elements = document.querySelectorAll("button.ajax-pagination-btn[type='submit'][data-disable-with]");
+        let counter = 1;
+    
+        while (elements.length > 0)
+        {
+            console.log("Expansion round " + counter++);
+    
+            for (let i = 0; i < elements.length; i++)
+            {
+                elements[i].click();
+            }
+            
+            let loadingItems = document.querySelectorAll('button.ajax-pagination-btn[type="submit"][data-disable-with][disabled]');
+            while (loadingItems.length > 0) {
+                await new Promise((resolve) => setTimeout(resolve, 2000));
+                loadingItems = document.querySelectorAll('button.ajax-pagination-btn[type="submit"][data-disable-with][disabled]');
+            }
+            
+            console.log("Refreshing elements...");
+            elements = document.querySelectorAll('button.ajax-pagination-btn[type="submit"][data-disable-with]');
+        }
+    
+        console.log("Complete!");
+    }
+    
+    async function HideResolved()
+    {
+        const elements = document.querySelectorAll("details[data-resolved='true']");
+        elements.forEach(function(element) { element.setAttribute("style", "display: none !important;"); });
+    }
+    
+    async function ShowResolved()
+    {
+        const elements = document.querySelectorAll("details[data-resolved='true']");
+        elements.forEach(function(element) { element.removeAttribute("style"); });
+    }
+    
+    async function HideCommits()
+    {
+        const elements = document.querySelectorAll("div.js-commit-group");
+        elements.forEach(function(element) { element.setAttribute("style", "display: none !important;"); });
+    }
+    
+    async function ShowCommits()
+    {
+        const elements = document.querySelectorAll("div.js-commit-group");
+        elements.forEach(function(element) { element.removeAttribute("style"); });
+    }
+    
+    async function HideStatuses() {
+        const elements = document.querySelectorAll(".js-timeline-item  .TimelineItem[data-team-hovercards-enabled]:not([data-url]), .js-timeline-item > div > div[id|=pullrequestreview] > .TimelineItem:not([data-url]), .js-timeline-item > .TimelineItem:not([data-url]) > span.TimelineItem-badge.bg-blue.text-white");
+        elements.forEach(function(element) { element.setAttribute("style", "display: none !important;"); });
+    }
+    
+    async function ShowStatuses() {
+        const elements = document.querySelectorAll(".js-timeline-item  .TimelineItem[data-team-hovercards-enabled]:not([data-url]), .js-timeline-item > div > div[id|=pullrequestreview] > .TimelineItem:not([data-url]), .js-timeline-item > .TimelineItem:not([data-url]) > span.TimelineItem-badge.bg-blue.text-white");
+        elements.forEach(function(element) { element.removeAttribute("style"); });
+    }
+    
+    async function HideComments() {
+        const elements = document.querySelectorAll(".TimelineItem.js-comment-container, .TimelineItem > .timeline-comment-group");
+        elements.forEach(function(element) { element.setAttribute("style", "display: none !important;"); });
+    }
+    
+    async function ShowComments() {
+        const elements = document.querySelectorAll(".TimelineItem.js-comment-container, .TimelineItem > .timeline-comment-group")
+        elements.forEach(function(element) { element.removeAttribute("style"); });
+    }
+
+    return {
+        Expand: Expand,
+        HideResolved: HideResolved,
+        ShowResolved: ShowResolved,
+        HideCommits: HideCommits,
+        ShowCommits: ShowCommits,
+        HideStatuses: HideStatuses,
+        ShowStatuses: ShowStatuses,
+        HideComments: HideComments,
+        ShowComments: ShowComments,
+        
+    };
+})();
